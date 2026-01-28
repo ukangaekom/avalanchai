8bde465f383f4cb93319a517db83e7a009d1a4b8
ukangaekom <ekomabasiuk@gmail.com>
2026-01-28 08:54:15 +0100

    added functions for aave protocol
    Started enabling onchainGPT to go protocol specific. Starting with AAVE protocol. I implemented getters function for aave token gateway

--- src/tools/tools_map.rs
+++ src/tools/tools_map.rs
@@ -1,4 +1,5 @@
 use crate::tools::wrappers::getters::getters::*;
+use crate::services::getter::protocols::*;
 use std::collections::HashMap;
 use std::pin::Pin;
 use std::future::Future;
@@ -23,5 +24,14 @@ pub static TOOLS: Lazy<HashMap<&'static str, AsyncFn>> = Lazy::new(||{
     m.insert("get_nft_balance_mainnet", Arc::new(|args| Box::pin(get_nft_balance_mainnet_wrapper(args))));
     m.insert("get_nft_details_mainnet", Arc::new(|args| Box::pin(get_nft_details_mainnet_wrapper(args))));
     m.insert("get_nft_total_supply_mainnet", Arc::new(|args| Box::pin(get_nft_total_supply_mainnet_wrapper(args))));
+
+
+
+    // AAVE TOOL Mappings
+    m.insert("aave_wrapped_token_get_wrappedethaddress_token", Arc::new(|args| Box::pin(aave::aave_wrapped_token_get_wrappedethaddress_token(args[0]))));
+    m.insert("aave_wrapped_token_get_weth_address", Arc::new(|args| Box::pin(aave::aave_wrapped_token_get_weth_address(args[0]))));
+    m.insert("aave_wrapped_token_get_pool_address", Arc::new(|args| Box::pin(aave::aave_wrapped_token_get_pool_address(args[0]))));
+    m.insert("aave_wrapped_token_get_owner", Arc::new(|args| Box::pin(aave::aave_wrapped_token_get_owner(args[0]))));
+
     m
 });
